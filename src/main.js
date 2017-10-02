7a48093e79d85601f0e3a38ade87ad2c3d968bc7
APA42 <alberto.p.42@gmail.com>
2017-10-02 21:11:23 +0200

    Another way to link the click button and the action

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
+"use strict";
 
 
+var playWithListBtn = document.getElementById("playing-with-list-btn");
 
 function playingWithList() {
 
@@ -15,3 +17,5 @@ function playingWithList() {
     }
 }
 
+
+playWithListBtn.addEventListener('click', playingWithList);
